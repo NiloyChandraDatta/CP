5d96c8697d6484863f9454da94ceeb019d798d16
Niloy Chandra Datta <niloycd98@gmail.com>
2026-01-02 11:42:04 +0600

    Create A - Blackslex and Password.cpp

---
+++ Codeforces/A - Blackslex and Password.cpp
@@ -0,0 +1,22 @@
+// https://codeforces.com/contest/2179/problem/A
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+void solve()
+{
+    int k, x; cin>>k>>x;
+    cout << k * x+1 << "\n";
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int tc; cin >> tc;
+    while (tc--)
+    {
+        solve();
+    }
+    return 0;
+}
